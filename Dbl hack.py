7f4ce56503b46155919a6ca70d42c309b400bfed
darklpro79 <132942429+darklpro79@users.noreply.github.com>
2023-05-08 20:48:58 +0200

    Create Dbl hack.py

---
+++ Dbl hack.py
@@ -0,0 +1,13 @@
+```
+# Python code to generate 379473 diamonds in Dragon Ball Legends on iOS 16
+
+# Import necessary libraries
+import random
+
+# Set the number of diamonds to generate
+diamonds = 379473
+
+# Generate the diamonds
+for i in range(diamonds):
+    print(random.randint(1, 100))
+```
