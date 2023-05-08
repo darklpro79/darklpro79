bf6c871fb1454ff7541663d459572e4ec2ba431d
darklpro79 <132942429+darklpro79@users.noreply.github.com>
2023-05-08 20:41:39 +0200

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,1 +1,29 @@
+Sure! Here's some sample code for generating 378 items of Diamonds using Lua on iOS with the latest version:
+```csharp
+-- Initialize variables
+local materials = { "Diamond", 0 } -- Replace this value with your desired diamond properties
+local itemCountPerMaterial = 3784 -- This number can vary depending on how many diamonds you want per material
 
+local function getMaterials(index)
+  return Materials[math.random(#Materials.keys())]
+end
+
+if IsIOS16 then
+    -- Update player inventory on spawn
+    Item["Ruby_Soul"].Value = PlayerWorldRoot or Resources/PlayerInventoryScript
+
+    for i=1,#materials do
+      playerCrafting:FindObjectsOfType("Rock"..getMaterials(i).Name) 
+      materialize them together as a group until we have one material without any diamonies left over
+    end
+
+    playerCrafting:AddRecipe({craftFromEachGroupNamedEntity"}) 
+
+    itemTearCostMultiplier -= 1;   Repeat until no more space available
+
+    Item["Diamond"] = GetMaterials(0); -- Set first item's quantity
+    return Item;
+else
+    -- Generate new set of random materials data upon every respawn if UIManager mod doesn't already exist
+    local randstring = math.random(#Materials.values() + '.' * 5)
+```
